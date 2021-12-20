6c8a9640545510aa17dc7f9936ca7021e1cc9225
rajkumar211999 <93135543+rajkumar211999@users.noreply.github.com>
2021-12-20 19:58:49 +0530

    Add files via upload

---
+++ Core java/String1.java
@@ -0,0 +1,11 @@
+package com.java.string;
+
+public class String1 
+{
+public static void main(String[] args) 
+ {
+String str1 = "Hello World";
+int size = str1.length();
+System.out.println(size);
+ }
+}
---
+++ Core java/StringBuffer1.java
@@ -0,0 +1,11 @@
+package com.java.string;
+
+public class StringBuffer1 {
+    public static void main(String[] args) {
+  StringBuffer s = new StringBuffer("StringBuffer");
+  s.append(" ,is a peer class of string ");
+  s.append(" ,that provides such of , ");
+  s.append("the functionality of strings ");
+  System.out.println(s);
+  }
+    }
